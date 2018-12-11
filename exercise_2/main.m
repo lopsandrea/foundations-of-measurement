ee67652243ced706c92d80c8989e5a7a845ef325
Paolo Rotolo <paolorotolo@ubuntu.com>
2018-12-11 13:54:06 +0100

    ADd things

--- exercise_2/main.m
+++ exercise_2/main.m
@@ -1,12 +1,12 @@
 function main
-mediana('Dati_1.txt');
 
+file1 = 'Dati_1.txt';
 
-
-    
+interquartile(file1);
 
     function v = parseFile(filename)
-        v = importdata(filename,' ');
+        data = importdata(filename,' ');
+        v = sort(data);
     end
 
     function v = istogramma(filename)
@@ -17,5 +17,31 @@ mediana('Dati_1.txt');
     function mediana(filename)
          v = num2str(median(parseFile(filename)));
          disp(['La mediana è ', v]);
+    end
+
+    function output = dispersione(filename)
+        output = range(parseFile(filename));
+        disp(['La dispersione è ' num2str(range(parseFile(filename)))]);
+    end
+
+    function output = firstQuartile(filename)
+        y = parseFile(filename);
+        output = median(y(y<median(y)));
+        disp(['Il primo quartile è ' num2str(output)]);
+    end
+    
+    function secondQuartile(filename)
+        output = median(parseFile(filename));
+        disp(['Il secondo quartile è ' num2str(output)]);
+    end
+
+    function output = thirdQuartile(filename)
+        y = parseFile(filename);
+        output = median(y(find(y>median(y))));
+    end
+
+    function interquartile(filename)
+       interquartile = thirdQuartile(filename) - firstQuartile(filename);
+       disp(['Lo scarto è ' num2str(interquartile)]);
     end
 end
