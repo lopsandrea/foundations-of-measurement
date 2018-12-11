b56d4f43660b47ad98a125fa921828ae90c6d108
Paolo Rotolo <paolorotolo@ubuntu.com>
2018-12-11 12:39:11 +0100

    Add mediana

--- exercise_2/main.m
+++ exercise_2/main.m
@@ -1,9 +1,21 @@
 function main
+mediana('Dati_1.txt');
 
-bar(parseFile('Dati_1.txt'));
+
 
     
+
     function v = parseFile(filename)
         v = importdata(filename,' ');
+    end
+
+    function v = istogramma(filename)
+        % calcola il passo qui
+        v = histogram(parseFile(filename));
+    end
+
+    function mediana(filename)
+         v = num2str(median(parseFile(filename)));
+         disp(['La mediana è ', v]);
     end
 end
